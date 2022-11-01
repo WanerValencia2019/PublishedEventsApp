8ccecf289274141a090cc29275629d01ec833f95
WanerValencia <waner.valencia@antpack.co>
2022-10-31 22:24:39 -0500

    fix

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -3,32 +3,21 @@
  * https://reactnavigation.org/docs/getting-started
  *
  */
-import { FontAwesome } from '@expo/vector-icons';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer, DefaultTheme, DarkTheme } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import * as React from 'react';
-import { ColorSchemeName, Pressable } from 'react-native';
+import { ColorSchemeName } from 'react-native';
 
-import Colors from '../constants/Colors';
-import useColorScheme from '../hooks/useColorScheme';
-import ModalScreen from '../screens/ModalScreen';
-import NotFoundScreen from '../screens/NotFoundScreen';
-import TabOneScreen from '../screens/TabOneScreen';
-import TabTwoScreen from '../screens/TabTwoScreen';
-import { RootStackParamList, RootTabParamList, RootTabScreenProps } from '../../types';
+import { RootStackParamList } from '../../types';
 import LinkingConfiguration from './LinkingConfiguration';
 import Login from '../screens/auth/Initial';
-import BottomTabs from './initialNavigation';
 import DrawerNav from './drawer';
 import Auth from '../screens/auth/Initial';
 import EventDetail from '../screens/eventDetail';
-import ShowTicketTypes from "../screens/showTicketTypes";
 import ViewTickets from '../screens/viewTickets';
 import ConfirmInfoBuy from '../screens/ConfirmInfoBuy';
 import PayWebView from '../screens/PayWebview';
 import ComingSoon from '../screens/ComingSoon';
-import Calendar from '../screens/calendar';
 
 export default function Navigation({ colorScheme }: { colorScheme: ColorSchemeName }) {
   return (
